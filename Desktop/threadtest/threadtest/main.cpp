403e858ae18cdef5b4aa2772f6ba035a04a8b410
Gordon <gojones@umich.edu>
2014-02-02 14:39:13 -0500

    first commit

---
+++ Desktop/threadtest/threadtest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  threadtest
+//
+//  Created by Gordon Jones on 9/24/13.
+//  Copyright (c) 2013 Gordon Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
